a4723a7ddb6927ecea65f98196028a6eea255c0b
BillyBooglyBob <123722533+BillyBooglyBob@users.noreply.github.com>
2024-03-07 21:36:29 +1000

    Created api route for viewing a single listing by id

--- backend/controllers/listing.controller.js
+++ backend/controllers/listing.controller.js
@@ -1,14 +1,37 @@
+import mongoose from "mongoose"
 import Listing from "../models/listing.model.js"
 
+// Create a listing
 export const createListing = async (req, res) => {
-
     try {
+        // use the current signed in user's id as the seller
         const inputs = { ...req.body, seller: req.user.id }
-        console.log("entered inputs:". inputs)
         const newListing = await Listing.createNewListing(inputs)
 
         res.status(200).json(newListing)
     } catch (error) {
         res.status(400).json({ error: error.message })
+    }
+}
+
+// Get a single listing by its id
+export const getListing = async (req, res) => {
+    try {
+        const listingID = req.params.id
+
+        // check if the id is a mongoDB id
+        if (!mongoose.Types.ObjectId.isValid(listingID)) {
+            return res.status(404).json({ error: 'Invalid ID' })
+        }
+
+        const listing = await Listing.findById(listingID)
+
+        if (!listing) {
+            return res.status(404).json({ error: 'Listing does not exist' })
+        }
+
+        res.status(200).json(listing)
+    } catch (error) {
+        res.status(500).json({error: error.message})
     }
 }
--- backend/routes/listing.route.js
+++ backend/routes/listing.route.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { createListing } from "../controllers/listing.controller.js";
+import { createListing, getListing } from "../controllers/listing.controller.js";
 import { checkToken } from "../utils/checkToken.js";
 
 const router = express.Router()
 
 router.post('/', checkToken, createListing)
+
+// Doesn't need to check token as everyone should be able to view listings
+router.get('/:id', getListing)
 
 export default router
