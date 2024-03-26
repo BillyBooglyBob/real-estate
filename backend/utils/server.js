9434a96c822b11f4369251642323166124c28f0d
BillyBooglyBob <123722533+BillyBooglyBob@users.noreply.github.com>
2024-03-26 15:46:59 +1000

    Add code to run app on render

--- backend/utils/server.js
+++ backend/utils/server.js
@@ -3,22 +3,31 @@ import mongoose from "mongoose"
 import cookieParser from "cookie-parser"
 import authRouter from '../routes/auth.route.js'
 import listingRouter from '../routes/listing.route.js'
+import path from 'path'
 
 // creates the server with all the middlewares and routers
 const createServer = () => {
     mongoose.connect(process.env.DB_URL)
-    .then(() => {
-        console.log('Connected to mongoDB')
-    })
-    .catch((err) => {
-        console.log(err)
-    })
+        .then(() => {
+            console.log('Connected to mongoDB')
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+
+    const __dirname = path.resolve()
 
     const app = express()
 
     // middlewares
     app.use(express.json())
     app.use(cookieParser());
+
+    app.use(express.static(path.join(__dirname, '/frontend/dist')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+    })
 
     // display request made to the api
     app.use((req, res, next) => {
